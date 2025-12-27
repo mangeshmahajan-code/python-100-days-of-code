6dd5023c2fbd4abf2cdd2803e423f21a31d261de
mangesh_mahajan <mangeshmahajan948@gmail.com>
2025-12-27 13:53:55 +0530

    Fix bugs in password and PIN generator

--- passwordgen.py
+++ passwordgen.py
@@ -1,22 +1,30 @@
 import random
-print("press :\n1 for password generator\n2 for pin generator")
-choice = input("Enter your choice: ")
-if choice==1:
- print ("password generator")
- input= input("Enter the length of the password: ")
- length = int(input)
- characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&"
- password = "".join(random.choice(characters) for i in range(length))
- print("Generated password:", password)
- print("Thank you for using the password generator!")
+
+print("Press:")
+print("1 for password generator")
+print("2 for pin generator")
+
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    print("Password Generator")
+    length = int(input("Enter the length of the password: "))
+
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&"
+    password = "".join(random.choice(characters) for _ in range(length))
+
+    print("Generated password:", password)
+    print("Thank you for using the password generator!")
+
+elif choice == 2:
+    print("PIN Generator")
+    length = int(input("Enter the length of the pin: "))
+
+    characters = "0123456789"
+    pin = "".join(random.choice(characters) for _ in range(length))
+
+    print("Generated pin:", pin)
+    print("Thank you for using the pin generator!")
+
 else:
- print ("pin generator")
- input= input("Enter the length of the pin: ")
- length = int(input)
-
- characters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
- pin = "".join(random.choice(characters) for i in range(length))
- print("Generated pin:", pin)
- print("Thank you for using the pin generator!")
- 
+    print("Invalid choice!")

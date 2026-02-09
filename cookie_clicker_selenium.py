e095c76f0dfcd2b3634daf57809ef3611693c4cd
mangesh_mahajan <mangeshmahajan948@gmail.com>
2026-02-09 19:09:55 +0530

    modified the cookie_clicker_selenium

--- cookie_clicker_selenium.py
+++ cookie_clicker_selenium.py
@@ -6,6 +6,7 @@ from time import time,sleep
 chrome_oprions =webdriver.ChromeOptions()
 chrome_oprions.add_experimental_option("detach",True)
 
+# selenium setup and open cookie clicker game
 driver = webdriver.Chrome(options=chrome_oprions)
 driver.get("https://ozh.github.io/cookieclicker/")
 sleep(2)
@@ -15,11 +16,11 @@ sleep(2)
 
 button = driver.find_element(By.ID,value="bigCookie")
 
-
 wait_time = 5
 timeout = time() + wait_time
 five_min = time() + 60*5
 
+# main loop to click cookie and buy items every 5 seconds, also prints cookie count and bought items
 while True:
     button.click()
 
@@ -28,13 +29,12 @@ while True:
             cookies_element = driver.find_element(By.ID, value="cookies")  
             cookie_text = cookies_element.text
             cookie_count = int(cookie_text.split()[0].replace(",",""))
-            print(cookie_count)
+            print(f"cookie_count: {cookie_count}")
 
-            # print(products)
+# find the most expensive item that can be bought and click it
             best_item = None
             for i in range(18,-1,-1):
                 product = driver.find_element(by=By.ID, value=f"product{i}")
-                # print(product.get_attribute("class"))
                 
                 if "product unlocked enabled" in product.get_attribute("class"):
                         best_item = product
@@ -48,8 +48,8 @@ while True:
         except(ValueError):
             print("Couldn't find cookie count or items")
         timeout= time()+ wait_time
-             
 
+# check if 5 minutes have passed, if so print final cookie count and break loop
     if time() > five_min:
             try:
                 cookies_element = driver.find_element(by=By.ID, value="cookies")
